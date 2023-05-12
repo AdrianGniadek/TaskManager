3fa0060ed4e9b0e996046f6a829aab4eed24237f
AdrianGniadek <adrian.gniadek03@gmail.com>
2023-05-12 10:54:03 +0200

    Metoda wyświetlająca opcje dostępne w programie

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,18 @@
 package pl.coderslab;
 
+
 public class TaskManager {
+    static String[][] tasks;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        displayOptions();
+    }
+
+    public static void displayOptions() {
+        System.out.println(ConsoleColors.BLUE + "Please select an option: " + ConsoleColors.RESET);
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
     }
 }
